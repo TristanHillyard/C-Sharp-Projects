bb003e83664d045f5680dd11d440d54316bc7b20
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-12 14:37:13 -0700

    Add files via upload
    assignments

---
+++ Stringy/Stringy/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+
+namespace Stringy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string name = "Tristan";
+            //string quote = "The man said, \"Hello\", Tristan. \n Hello on a new line. \n \t Hello on a tab.";
+            //string fileName = @"C: \Users\Tristan";
+
+            //bool trueOrFalse = name.Contains("s");
+            //Console.WriteLine(trueOrFalse);
+            //trueOrFalse = name.EndsWith("s");
+
+            //int length = name.Length;
+
+            //name = name.ToUpper();
+
+            //Console.WriteLine(fileName);
+            //Console.WriteLine(quote);
+            //Console.ReadLine();
+
+            string name = "Tristan";
+            name = "Joe";
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Jesse");
+
+            Console.WriteLine(sb);
+
+            Console.WriteLine(name);
+            Console.ReadLine();
+        }
+    }
+}

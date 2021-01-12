2bafa0617d0f300efdabaf975f085b497f9ffb86
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-12 14:48:33 -0700

    Add files via upload
    assignments

---
+++ VariablesandDataTypes/VariablesandDataTypes/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace VariablesandDataTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is a simple program
+            //Console.WriteLine("What is your name?");
+            //string yourName = Console.ReadLine();
+            //Console.WriteLine("Your name is: " + yourName);
+            //Console.ReadLine();
+
+            //Console.WriteLine("What is your favorite number?");
+            //string favoriteNumber = Console.ReadLine();
+            //int favNum = Convert.ToInt32(favoriteNumber);
+            //int total = favNum + 5;
+            //Console.WriteLine("Your favorite number plus 5 is: " + total);
+            //Console.ReadLine();
+
+            //bool isStudying = false;
+            //byte hoursWorked = 42;
+            //sbyte currentTempeture = -23;
+            //char questionMark = '\u2103';
+
+
+            //double heightInCentimeters = 211.30202092;
+
+            //float secondsLeft = 2.62f;
+
+            //short tempetureOnMars = -341;
+
+            //string myName = "Tristan";
+
+            //int curruntAge = 30;
+            //string yearsOld = curruntAge.ToString();
+
+            //bool isRaining = true;
+            //string rainingStatus = Convert.ToString(isRaining);
+            //Console.WriteLine(rainingStatus);
+            //Console.ReadLine();
+
+            //Console.WriteLine(questionMark);
+            //Console.ReadLine();
+
+            //int num1 = 3;
+            //int num2 = 4;
+            //Console.WriteLine(num1 + num2);
+
+            //int num3 = 5;
+            //int num4 = 8;
+            //Console.WriteLine(num3 - num4);
+
+            //int num5 = 3;
+            //int num6 = 9;
+            //Console.WriteLine(num5 * num6);
+
+            //int num7 = 5;
+            //int num8 = 4;
+            //Console.WriteLine(num7 - num8);
+
+            string newName = "Tristan";
+            string oldName = "Jax";
+            Console.WriteLine(newName + oldName);
+        }
+    }
+}
---
+++ VoidMethod/VoidMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace VoidMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            newVoid math = new newVoid();
+            int num = 10;
+            int num2 = 5;
+            math.NewVoid(num, num2);
+            math.NewVoid(12, 20);
+        }
+    }
+}

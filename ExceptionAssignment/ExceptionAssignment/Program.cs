f76e17716846683ab94ff7adb754973305d09ab3
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-12 14:05:44 -0700

    Add files via upload
    assignment

---
+++ ExceptionAssignment/ExceptionAssignment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExceptionAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> newNum = new List<int> { 33, 44, 50, 100 };
+            Console.WriteLine("Enter a number to divide by.");
+            
+            try
+            {
+                int numResult = Convert.ToInt32(Console.ReadLine());
+                foreach (int num in newNum)
+                {
+                    int divNum = num / numResult;
+                    Console.WriteLine(divNum);
+                    Console.ReadLine();
+                }
+            }
+            catch(DivideByZeroException ex)
+            {
+                Console.WriteLine("Unable to divide these numbers by Zero.");
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine("Cannot divide letter format.");
+            }
+            Console.WriteLine("The program has finished the try/catch block.");
+        }
+    }
+}

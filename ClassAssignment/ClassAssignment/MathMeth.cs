f83e098f7ce1c4019b81971de0afc724a3e22343
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-12 14:02:24 -0700

    Add files via upload
    assignments

---
+++ ClassAssignment/ClassAssignment/MathMeth.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassAssignment
+{
+    public class MathMeth
+    {
+        public int Math(int num1)
+        {
+            int result;
+            result = num1 * 2;
+            return result;
+
+        }
+        public int MathA(int num2)
+        {
+            int result;
+            result = num2 + 10;
+            return result;
+        }
+        public int MathS(int num3)
+        {
+            int result;
+            result = num3 - 4;
+            return result;
+        }
+    }
+}
---
+++ ClassAssignment/ClassAssignment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassAssignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1;
+            Console.WriteLine("What number do you want to do math operations on?");
+            num1 = Convert.ToInt32(Console.ReadLine());
+            MathMeth math = new MathMeth();
+            math.Math(num1);
+            Console.WriteLine(math.Math(num1));
+            math.MathA(num1);
+            Console.WriteLine(math.MathA(num1));
+            math.MathS(num1);
+            Console.WriteLine(math.MathS(num1));
+            
+             
+        }
+    }
+}
